7866b955288782b0f50e3a0a3a762a3d195417f5
Joel Gabriel <joelybahh@gmail.com>
2016-07-06 14:22:46 +1000

    All project files
    first commit

---
+++ Death Defence/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+    private static GameManager instance;
+    public static GameManager Instance {
+        get {
+            if (instance == null) {
+                GameObject go = new GameObject("GameManager");
+                go.AddComponent<GameManager>();
+            }
+
+            return instance;
+        }
+    }
+
+    public int score { get; set; }
+    public int day { get; set; }
+    public int enemiesKilled { get; set; }
+    public int roundEnemyCount { get; set; }
+    public float timeAlive { get; set; }
+    public bool isDead { get; set; }
+    public bool betweenRounds { get; set; }
+    public bool isReloading { get; set; }
+
+    void Awake() {
+        instance = this;
+        betweenRounds = false;
+        isReloading = false;
+    }
+}
---
+++ Death Defence/Assets/Scripts/GunZoom.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunZoom : MonoBehaviour {
+
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(1) && !anim.GetBool("ZoomIn")) {
+            anim.SetBool("ZoomIn", true);
+            anim.SetBool("ZoomOut", false);
+        } 
+
+        if (Input.GetMouseButtonUp(1)) {
+            anim.SetBool("ZoomOut", true);
+            anim.SetBool("ZoomIn", false);
+        }
+    }
+}
